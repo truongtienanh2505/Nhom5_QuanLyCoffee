c50d95f9f774cca2e2dbc77b87d5bedf21fdca96
Nguyễn Nhật Long <nhatlong12112005@gmail.com>
2025-11-21 12:32:06 +0700

    Repositories

---
+++ QuanLyCafe.DAL/DatabaseConfig.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace QuanLyCafe.DAL
+{
+   
+    public static class DatabaseConfig
+    {
+       
+        public static string ConnectionString =
+            "Server=localhost;Database=QuanLyCoffee;User Id=sa;Password=123456;TrustServerCertificate=True;";
+
+      
+        public static SqlConnection GetConnection()
+        {
+            var conn = new SqlConnection(ConnectionString);
+            conn.Open();
+            return conn;
+        }
+    }
+}
